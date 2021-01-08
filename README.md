9dfd89e667800f239a5ff4577d09697d36b0793c
Douglas Dohmeyer <rdohmeyer@gmail.com>
2021-01-07 22:25:09 -0600

    Updated README and added configuration parameters for tile dimensions and scaling factor.

--- README.md
+++ README.md
@@ -3,7 +3,8 @@
 This is the help string:
 ```
 usage: __main__.py [-h] [-i INPUT_IMAGE] [-t IMAGE_TABLE] [-o OUTPUT_IMAGE]
-                   [-d IMAGE_DIRECTORY]
+                   [-d IMAGE_DIRECTORY] [-hxw TILE_DIMENSIONS]
+                   [-S SCALING_FACTOR]
 
 optional arguments:
   -h, --help            show this help message and exit
@@ -16,15 +17,35 @@ optional arguments:
                         The path where the output image will be put.
   -d IMAGE_DIRECTORY, --image_directory IMAGE_DIRECTORY
                         The directory under which images will be searched.
-                        Currently, the search algorithm will match only files
-                        with one of the following extensions tif, tiff, png,
-                        bmp, jpg, and jpeg. It will search under the given
-                        directory and all subdirectories.
+                        This will generate a .csv file with the same name as
+                        the directory on which you've run the utility.This
+                        resultant csv will be your image_table to be used in
+                        generating mosaics.Currently, the search algorithm
+                        will match only files with one of the following
+                        extensions tif, tiff, png, bmp, jpg, and jpeg. It will
+                        search under the given directory and all
+                        subdirectories.
+  -hxw TILE_DIMENSIONS, --tile_dimensions TILE_DIMENSIONS
+                        The width and height (in pixels) of each tile in the
+                        mosaic. This should be represented as "HxW" where W is
+                        the width and H is the height (e.g. 640x480).Larger
+                        dimensions result in larger mosaic tiles while smaller
+                        dimensions result in smaller mosaic tiles.
+  -S SCALING_FACTOR, --scaling_factor SCALING_FACTOR
+                        The multiple by which the input image will be resized
+                        (this is applied to both x and y axies). So, a
+                        scaling_factor of 0.5 will make both the x and y axies
+                        of the resultant image half the length of the input
+                        hence the resultant image will be one quarter of the
+                        area of the input image. The same applies for
+                        scaling_factors greater than 1 except the resultan
+                        image will be larger. This is especially useful for
+                        smaller images.
 ```
 
 To generate your source image table, simply run
 the program using the `-d` flag specifying the source directory
-under which you would like to generate this table from.
+where the images from under which will be used to generate this table.
 After generating the source table, the program will exit.
 
 The image table is required for generating an image mosaic.
--- mosaic/main.py
+++ mosaic/main.py
@@ -1,5 +1,6 @@
 from mosaic import image_consolodation
 from mosaic import image_tiling
+import re
 import argparse
 import os
 
@@ -10,8 +11,29 @@ def main(args):
     image_table = args.image_table
     output_image = args.output_image
     image_directory = args.image_directory
+    tile_dimensions = args.tile_dimensions
+    scaling_factor = args.scaling_factor
     for arg in args.__dict__:
         print(arg, args.__dict__[arg])
+
+    match = re.match(r'\d+x\d+', tile_dimensions)
+    if match:
+        s = match.group()
+        h, w = s.split('x')
+        tile_dimensions = (int(h), int(w))
+    else:
+        default = (30, 30)
+        print(f"Bad tile dimensions: {tile_dimensions}")
+        print(f"Resetting default: {default}")
+        tile_dimensions = default
+    if scaling_factor > 0:
+        pass
+    else:
+        default = 1
+        print(f"Bad scaling factor: {scaling_factor}\n Expected a float greater than zero.")
+        print(f"Resetting default: {default}")
+        scaling_factor = default
+
     if image_directory:
         basename = os.path.basename(image_directory)
         computer = image_consolodation.ColorStatisticComputer(
@@ -23,14 +45,18 @@ def main(args):
     else:
         timmy = image_tiling.TiledImage(
                 image_path=input_image, 
-                tile_shape=(20,20), 
-                scaling_factor=2)
+                tile_shape=tile_dimensions, 
+                scaling_factor=scaling_factor)
         image_df = image_tiling.pd.read_csv(image_table)
         out_image_matrix = timmy.make(image_df, unique_flag=False)
         image_tiling.cv2.imwrite(output_image, out_image_matrix)
 
+
+
 def parse_arguments():
     # parse arguments for the image tiler
+
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
             '-i', 
@@ -57,10 +83,36 @@ def parse_arguments():
             '--image_directory',
             type=str,
             help=('The directory under which images will be searched. '
+                'This will generate a .csv file with the same name as the directory on which you\'ve '
+                'run the utility.'
+                'This resultant csv will be your image_table to be used in generating mosaics.'
                 'Currently, the search algorithm will match only files with '
                 'one of the following extensions tif, tiff, png, bmp, jpg, and jpeg. '
                 'It will search under the given directory and all subdirectories.'
                 )
+            )
+    parser.add_argument(
+            '-hxw',
+            '--tile_dimensions',
+            type=str,
+            help=('The width and height (in pixels) of each tile in the mosaic. '
+                'This should be represented as "HxW" where W is the width and H is the height '
+                '(e.g. 640x480).'
+                'Larger dimensions result in larger mosaic tiles while smaller dimensions result in smaller mosaic tiles.'
+                ),
+            default='30x30'
+            )
+    parser.add_argument(
+            '-S',
+            '--scaling_factor',
+            type=float,
+            help=('The multiple by which the input image will be resized (this is applied to both x and y axies). '
+                'So, a scaling_factor of 0.5 will make both the x and y axies of the resultant image half the length of the input '
+                'hence the resultant image will be one quarter of the area of the input image. '
+                'The same applies for scaling_factors greater than 1 except the resultan image will be larger. '
+                'This is especially useful for smaller images. '
+                ),
+            default=1
             )
     args = parser.parse_args()
     main(args)
